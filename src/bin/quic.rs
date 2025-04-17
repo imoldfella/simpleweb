ba5da53b2d22e70d0dabe2e15f975fc58857d773
Jim Hurd <jimh@datagrove.com>
2025-04-17 18:25:39 -0400

    a

--- src/bin/quic.rs
+++ src/bin/quic.rs
@@ -1,6 +1,9 @@
+use log::*;
 use mio::net::UdpSocket;
 use mio::{Events, Interest, Poll, Token};
 use quiche::{Connection, ConnectionId};
+use ring::rand::SystemRandom;
+use simpleweb::quiche::{ClientIdMap, ClientMap};
 use std::{collections::HashMap, net::SocketAddr, time::Instant};
 
 const QUIC_TOKEN: Token = Token(usize::MAX - 1);
@@ -37,60 +40,114 @@ fn main() -> std::io::Result<()> {
     config.set_initial_max_stream_data_bidi_local(1_000_000);
     config.set_initial_max_streams_bidi(100);
 
+    let rng = SystemRandom::new();
+    let conn_id_seed =
+        ring::hmac::Key::generate(ring::hmac::HMAC_SHA256, &rng).unwrap();
+
+    let mut next_client_id = 0;
+    let mut clients_ids = ClientIdMap::new();
+    let mut clients = ClientMap::new();
+
+    let mut continue_write = false;
     loop {
+        // quiche server has timer check here, but implies they could do better.
+        // let timeout = match continue_write {
+        //     true => Some(std::time::Duration::from_secs(0)),
+
+        //     false => clients.values().filter_map(|c| c.conn.timeout()).min(),
+        // };
+
         poll.poll(&mut events, None)?;
 
-        for event in events.iter() {
-            if event.token() != QUIC_TOKEN {
-                continue;
+        'read: loop {
+            // If the event loop reported no events, it means that the timeout
+            // has expired, so handle it without attempting to read packets. We
+            // will then proceed with the send loop.
+            if events.is_empty() && !continue_write {
+                trace!("timed out");
+
+                //clients.values_mut().for_each(|c| c.conn.on_timeout());
+
+                break 'read;
             }
 
-            loop {
-                match socket.recv_from(&mut buf) {
-                    Ok((len, src)) => {
-                        let hdr = match quiche::Header::from_slice(
-                            &mut buf[..len],
-                            quiche::MAX_CONN_ID_LEN,
-                        ) {
-                            Ok(v) => v,
-                            Err(_) => continue,
-                        };
+            for event in events.iter() {
+                match event.token() {
+                    QUIC_TOKEN => {
+                        // loop until would bock.
+                        loop {
+                            match socket.recv_from(&mut buf) {
+                                Ok((len, src)) => {
+                                    let pkt_buf = &mut buf[..len];
+                                    let hdr = match quiche::Header::from_slice(
+                                        pkt_buf,
+                                        quiche::MAX_CONN_ID_LEN,
+                                    ) {
+                                        Ok(v) => v,
 
-                        let conn_id = hdr.dcid.clone();
-                        let conn_entry = connections.entry(conn_id.clone()).or_insert_with(|| {
-                            let scid = quiche::ConnectionId::from_ref(&hdr.dcid);
-                            let mut conn =
-                                quiche::accept(&scid, None, addr, src, &mut config).unwrap();
-                            while let Ok((write, send_info)) = conn.send(&mut out) {
-                                let _ = socket.send_to(&out[..write], src);
+                                        Err(e) => {
+                                            error!("Parsing packet header failed: {:?}", e);
+                                            continue 'read;
+                                        }
+                                    };
+
+                                    let conn_id = ring::hmac::sign(&conn_id_seed, &hdr.dcid);
+                                    let conn_id = &conn_id.as_ref()[..quiche::MAX_CONN_ID_LEN];
+                                    let conn_id : ConnectionId<'static>= conn_id.to_vec().into();
+
+
+
+                                }
+                                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
+                                Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => break,
+                                Err(e) => return Err(e),
                             }
-
-                            (conn, src)
-                        });
-
-                        let conn = &mut conn_entry.0;
-                        let recv_info = quiche::RecvInfo {
-                            from: src,
-                            to: addr,
-                        };
-                        let _ = conn.recv(&mut buf[..len], recv_info);
-
-                        // Handle application data
-                        while let Ok((stream_id, data)) = conn.stream_recv(0, &mut [0; 1024]) {
-                            println!("Received stream {}: {} bytes", stream_id, data);
-                            let _ = conn.stream_send(stream_id as u64, b"Hello from quiche!", true);
-                        }
-
-                        // Send outgoing packets
-                        while let Ok((write, send_info)) = conn.send(&mut out) {
-                            let _ = socket.send_to(&out[..write], conn_entry.1);
                         }
                     }
-                    Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => break,
-                    Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => break,
-                    Err(e) => return Err(e),
+                    _ => {
+                        warn!("unexpected event: {:?}", event);
+                        continue;
+                    }
                 }
             }
         }
     }
 }
+
+// let hdr = match quiche::Header::from_slice(
+//     &mut buf[..len],
+//     quiche::MAX_CONN_ID_LEN,
+// ) {
+//     Ok(v) => v,
+//     Err(_) => continue,
+// };
+
+// let conn_id = hdr.dcid.clone();
+// let conn_entry = connections.entry(conn_id.clone()).or_insert_with(|| {
+//     let scid = quiche::ConnectionId::from_ref(&hdr.dcid);
+//     let mut conn =
+//         quiche::accept(&scid, None, addr, src, &mut config).unwrap();
+//     while let Ok((write, send_info)) = conn.send(&mut out) {
+//         let _ = socket.send_to(&out[..write], src);
+//     }
+
+//     (conn, src)
+// });
+
+// let conn = &mut conn_entry.0;
+// let recv_info = quiche::RecvInfo {
+//     from: src,
+//     to: addr,
+// };
+// let _ = conn.recv(&mut buf[..len], recv_info);
+
+// // Handle application data
+// while let Ok((stream_id, data)) = conn.stream_recv(0, &mut [0; 1024]) {
+//     println!("Received stream {}: {} bytes", stream_id, data);
+//     let _ = conn.stream_send(stream_id as u64, b"Hello from quiche!", true);
+// }
+
+// // Send outgoing packets
+// while let Ok((write, send_info)) = conn.send(&mut out) {
+//     let _ = socket.send_to(&out[..write], conn_entry.1);
+// }
--- src/quiche/mod.rs
+++ src/quiche/mod.rs
@@ -2,3 +2,5 @@ pub mod app;
 pub use app::*;
 pub mod common;
 pub use common::*;
+pub mod server;
+pub use server::*;
