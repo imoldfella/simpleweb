3ccee35c7e515bf1bd66adfce3d50ba59a58c67c
jim <jimh@datagrove.com>
2025-04-23 10:52:49 -0400

    a

--- src/exec/mod.rs
+++ src/exec/mod.rs
@@ -1,21 +1,20 @@
-use std::{future::Future, pin::Pin, task::{Context, Poll}};
+use std::{
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
 
 //static mut IS_URING : bool = false;
 
-pub struct CountFuture {
-   
-}
+pub struct CountFuture {}
 
-impl CountFuture {
+impl CountFuture {}
 
-}
-
-pub struct OkFuture {
-}
+pub struct OkFuture {}
 
 // uring always uses i32 error? or mostly?
-type CountResult = std::result::Result<usize,i32>;
-type OkResult = std::result::Result<(),i32>;    
+type CountResult = std::result::Result<usize, i32>;
+type OkResult = std::result::Result<(), i32>;
 
 impl Future for CountFuture {
     type Output = CountResult;
@@ -25,18 +24,31 @@ impl Future for CountFuture {
     }
 }
 
-
 pub struct ThreadExec {
     is_uring: bool,
 }
 
-impl ThreadExec {
+impl ThreadExec {}
+
+// how should we code routines? pass thread explicitly, or use thread local storage?
+
+#[derive(Clone, Copy)]
+pub struct Os {
+    thr: *mut ThreadExec,
+}
+
+impl Os {
     pub fn read_some(
         &self,
-        thread: usize,
         connection: usize,
         buf: &[u8],
     ) -> Pin<Box<dyn Future<Output = CountResult>>> {
-        Box::pin(CountFuture{})
+        Box::pin(CountFuture {})
     }
+}
+
+pub async fn some_fn(os: Os) -> CountResult {
+    let buf = vec![0; 1024];
+    let result = os.read_some(&[]).await;
+    result
 }
