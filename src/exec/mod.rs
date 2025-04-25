6f70cac546210050f040897a35ae254a756e5a75
Jim Hurd <jimh@datagrove.com>
2025-04-24 20:28:52 -0400

    a

--- src/exec/mod.rs
+++ src/exec/mod.rs
@@ -1,12 +1,22 @@
 use std::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll},
+    future::Future, ops::Deref, pin::Pin, task::{Context, Poll}
 };
+
+use env_logger::Env;
+
+// probably want something like dashmap but with more memory control
 
 //static mut IS_URING : bool = false;
 
-pub struct ConnectionInner {}
+pub struct Ptr<T> {
+    ptr: *mut T,
+}
+impl<T> Deref for Ptr<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.ptr }
+    }
+}
 
 pub enum ConnectionType {
     Udp,
@@ -15,15 +25,37 @@ pub enum ConnectionType {
 
 pub struct User {}
 
+
+// we could restrict to environment, but passing the extra arguments allows them to stay in registers
+type Proc = fn(env: Ptr<Environment>,db: Ptr<Db>,  thr: Ptr<Thread>, cn: Ptr<Connection>) -> u32;
+// should we share these globally and deal with locks?
+// should we compile procedures dynamically or AOT?
+pub struct Iface {
+    pub proc: Box<[Proc]>,
+}
+// capabilities are injected into the environment
+pub struct Environment {
+    iface: Box<[u32]>,
+}
+
 // interface = schema or no? is there a better security language we can use?
 // each schema has multiple partitions; the user is authorized to access a subset of the partitions (like row level security) why not use the schema as the interface? we can always create
 pub struct Connection {
-    pub inner: *mut ConnectionInner,
     pub connection_type: ConnectionType,
     pub user: u32,
 
     // authorize connection, allows other rules than simply user (location, time)
-    pub interface: Box<[u32]>,
+    pub env: Box<[Environment]>,
+}
+
+pub struct Db {
+    pub user: Box<[User]>,
+    pub iface: Box<[Iface]>,
+    pub thread: Box<[Thread]>,
+}
+pub struct Thread {
+    is_uring: bool,
+    pub connection: Box<[Connection]>, 
 }
 
 pub struct CountFuture {}
@@ -44,20 +76,23 @@ impl Future for CountFuture {
     }
 }
 
-pub struct ThreadExec {
-    is_uring: bool,
-}
 
-impl ThreadExec {}
 
 // how should we code routines? pass thread explicitly, or use thread local storage?
 
-#[derive(Clone, Copy)]
-pub struct Os {
-    thr: *mut ThreadExec,
-}
+#[derive(Debug, thiserror::Error)]
+pub enum DbError {
 
-impl Os {
+
+    #[error("I/O error")]
+    Io(#[from] std::io::Error),
+
+    #[error("Invalid argument")]
+    InvalidArgument,
+}
+type DbResult<T> = std::result::Result<T, DbError>;
+
+impl Thread {
     pub fn read_some(
         &self,
         connection: Connection,
@@ -65,38 +100,52 @@ impl Os {
     ) -> Pin<Box<dyn Future<Output = CountResult>>> {
         Box::pin(CountFuture {})
     }
+    // maybe these should be on the connection? do they need the thread?
+    pub fn result_error(&self, connection: Connection, streamid: u64, error: i32) {}
+    pub fn result(&self, connection: Connection, streamid: u64, buf: &[u8], complete: bool) {
+        // the first packet in a stream must be at least 8 bytes, (aside from the stream id in the header)
+    }
+}
+fn read_u16(input: &[u8], range: std::ops::Range<usize>) -> Result<u16, DbError> {
+    input.get(range)
+         .and_then(|s| s.try_into().ok())
+         .map(u16::from_le_bytes)
+         .ok_or(DbError::InvalidArgument)
+}
 
-    pub fn request(&self, connection: Connection, streamid: u64, buf: &[u8], complete: bool) {
+fn read_u32(input: &[u8], range: std::ops::Range<usize>) -> Result<u32, DbError> {
+    input.get(range)
+         .and_then(|s| s.try_into().ok())
+         .map(u32::from_le_bytes)
+         .ok_or(DbError::InvalidArgument)
+}
+impl Db {
+
+    pub fn request(&self, thread: Ptr<Thread>, connection: Connection, streamid: u64, buf: &[u8], complete: bool) -> DbResult<()> {
         // the first packet in a stream must be at least 8 bytes, (aside from the stream id in the header)
         // probably don't need to check here?
         if buf.len() < 8 {
-            self.result_error(connection, streamid, -1);
-            return;
+            return Err(DbError::InvalidArgument); 
         }
-        // read 4 byte little endian schema and 4 byte little endian procid
-        let interface_handle = u32::from_le_bytes(buf[0..4].try_into().unwrap());
-        let procid = u32::from_le_bytes(buf[4..8].try_into().unwrap());
+        // read 4 byte little endian environment and 4 byte little endian procid
+        let env = read_u16(buf, 0..2)?;
+        let iface = read_u16(buf, 2..4)?;
+        let procid = read_u32(buf, 4..8)?;
 
-        if interface_handle > connection.interface.len() as u32 {
-            self.result_error(connection, streamid, -1);
-            return;
-        }
+        let env = self.env.get(env as usize).map(|| DbError::InvalidArgument)?;
         let interface = connection.interface[interface_handle as usize];
         if interface == 0 {
-            self.result_error(connection, streamid, -1);
+            thread.result_error(connection, streamid, -1);
             return;
         }
 
         // return error is schema.procid is not authorized.
     }
 
-    pub fn result_error(&self, connection: Connection, streamid: u64, error: i32) {}
-    pub fn result(&self, connection: Connection, streamid: u64, buf: &[u8], complete: bool) {
-        // the first packet in a stream must be at least 8 bytes, (aside from the stream id in the header)
-    }
+
 }
 
-pub async fn some_fn(os: Os, connection: Connection) -> CountResult {
+pub async fn some_fn(os: Thread, connection: Connection) -> CountResult {
     let buf = vec![0; 1024];
     let result = os.read_some(connection, &[]).await;
     result
@@ -105,7 +154,7 @@ pub async fn some_fn(os: Os, connection: Connection) -> CountResult {
 // an rpc will have some number of blobs; the final blob is the parameter block.
 // the initial blobs maybe stored to disk depending on memory pressure.
 //
-pub async fn read_rpc(os: Os, connection: Connection) -> CountResult {
+pub async fn read_rpc(os: Thread, connection: Connection) -> CountResult {
     let buf = vec![0; 1024];
     let result = os.read_some(connection, &buf).await;
     result
